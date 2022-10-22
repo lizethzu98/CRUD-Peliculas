284c9ae3913872f8331b23c869c6a46bb2960e4d
Lizeth Zuñiga <111620498+lizethzu98@users.noreply.github.com>
2022-10-22 10:41:51 -0500

    alertas

--- js/funciones.js
+++ js/funciones.js
@@ -4,12 +4,6 @@ function validaCampos() {
     var comentario = document.getElementById("cmbComentario").value;
     let calificacion = $('input[name="estrellas"]:checked').val();
     //alerta
-    alert(calificacion);
-    //ERROR
-    const div = document.createElement("div");
-    div.textContent = "Valor requerido*";
-    div.className = "texto-error";
-
     //Alertas
     var alertas = 0;
     const divUsuario = document.getElementById("divUsuario");
@@ -18,65 +12,98 @@ function validaCampos() {
     //Validar usuario
     if (usuario == '' || usuario == null || usuario.length <= 3) {
         if (alertas <= 0) {
+
+            let div = document.createElement("div");
+            div.textContent = "Valor requerido*";
+            div.className = "texto-error";
             divUsuario.appendChild(div);
-        }
-    }
-    else 
-    { 
-        if  (alertas == 1){
-                divUsuario.removeChild(divUsuario.lastChild);
 
         }
-       
     }
+    else {
+        if (alertas == 1) {
+            divUsuario.removeChild(divUsuario.lastChild);
+
+        }
+
+    }
+
     //alertas
+    alertas = 0;
     const divPeliculas = document.getElementById("divPeliculas");
     alertas = divPeliculas.getElementsByClassName('texto-error').length;
     //validar peliculas
-    if (pelicula == '' || pelicula == null || pelicula == 0 ){
-        if (alertas == 0){
+    if (pelicula == '' || pelicula == null || pelicula == 0) {
+        if (alertas == 0) {
+
+            let div = document.createElement("div");
+            div.textContent = "Valor requerido*";
+            div.className = "texto-error";
             divPeliculas.appendChild(div);
+
         }
     }
-    else{
-        if (alertas == 1 ){
+    else {
+        if (alertas == 1) {
             divPeliculas.removeChild(divPeliculas.lastChild);
         }
-         
+
 
     }
-
+    alertas = 0;
     const divComentarios = document.getElementById("divComentarios");
     alertas = divComentarios.getElementsByClassName('texto-error').length;
 
     //validar comentarios
 
-    if (comentario == '' || comentario == null){
-        if (alertas == 0 ){
+    if (comentario == '' || comentario == null) {
+        if (alertas == 0) {
+
+            let div = document.createElement("div");
+            div.textContent = "Valor requerido*";
+            div.className = "texto-error";
             divComentarios.appendChild(div);
+
         }
     }
-else {
-    if(alertas == 1 ){
-        divComentarios.removeChild(divComentarios.lastChild);
+    else {
+        if (alertas == 1) {
+            divComentarios.removeChild(divComentarios.lastChild);
+        }
     }
-}
 
-
-const divCalificacion = document.getElementById("divCalificacion");
+    alertas = 0;
+    const divCalificacion = document.getElementById("divCalificacion");
     alertas = divCalificacion.getElementsByClassName('texto-error').length;
-//validar calificacion
-if(calificacion == '' || calificacion == null || calificacion == 0){
-    if (alertas == 0 ){
-        divCalificacion.appendChild(div);
+    //validar calificacion
+    if (calificacion == '' || calificacion == null || calificacion == 0) {
+        if (alertas == 0) {
+
+            let div = document.createElement("div");
+            div.textContent = "Valor requerido*";
+            div.className = "texto-error";
+            divCalificacion.appendChild(div);
+        }
+    }
+    else {
+        if (alertas == 1) {
+            divCalificacion.removeChild(divCalificacion.lastChild);
+        }
+    }
+
+    var formulario = document.getElementById("formulario");
+    alertas = formulario.getElementsByClassName("texto-error").length;
+    if (alertas > 0) {
+        alert("error");
+    }
+    else {
+        alert("todo correcto");
+        var divNoComentario = document.getElementById("noComentario");
+        divNoComentario.remove();
     }
 }
-else{
-    if (alertas == 1){ 
-        divCalificacion.removeChild(divCalificacion.lastChild);
-    }
-}
 
+
+function imprimirComentario() {
 
 }
-
