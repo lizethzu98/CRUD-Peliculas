c04fd5443b5adb4bd6ef88c3b113d432bab44263
Lizeth Zuñiga <111620498+lizethzu98@users.noreply.github.com>
2022-10-22 23:15:07 -0500

    comentarios

--- js/funciones.js
+++ js/funciones.js
@@ -97,13 +97,87 @@ function validaCampos() {
         alert("error");
     }
     else {
-        alert("todo correcto");
+
         var divNoComentario = document.getElementById("noComentario");
-        divNoComentario.remove();
+        if (divNoComentario != null) {
+            divNoComentario.remove();
+        }
+        imprimirComentario();
+
+        document.getElementById("cmbNomUsuario").value = "";
+        document.getElementById("selectPeliculas").value = "0";
+        document.getElementById("cmbComentario").value = "";
+        var arRadioBtn = document.getElementsByName("estrellas");
+
+        for (var ii = 0; ii < arRadioBtn.length; ii++) {
+            var radButton = arRadioBtn[ii];
+            radButton.checked = false;
+        }
     }
 }
 
 
 function imprimirComentario() {
+    var usuario = document.getElementById("cmbNomUsuario").value;
+    var pelicula = document.getElementById("selectPeliculas");
+    var peliculaTexto = pelicula.options[pelicula.selectedIndex].text;
+    var comentario = document.getElementById("cmbComentario").value;
+    let calificacion = $('input[name="estrellas"]:checked').val();
+
+    var estrellas = "";
+
+
+    switch (calificacion) {
+        case '1':
+            estrellas = "★";
+            break;
+        case '2':
+            estrellas = "★★";
+
+            break;
+
+        case '3':
+            estrellas = "★★★";
+
+            break;
+        case '4':
+            estrellas = "★★★★";
+
+            break;
+        case '5':
+            estrellas = "★★★★★";
+            break;
+
+        default:
+            break;
+    }
+
+
+    
+
+    var coment = document.getElementById("accordion");
+
+    coment.innerHTML = "<div class='card'><div class='card-header' id='headingOne'>" +
+        "<h5 class='mb-0'>" +
+        "<button class='btn btn-link' data-toggle='collapse' data-target='#collapseOne' aria-expanded='true' aria-controls='collapseOne'>" +
+        peliculaTexto +
+        "</button>" +
+        "</h5>" +
+        "</div>" +
+        "<div id='collapseOne' class='collapse show' aria-labelledby='headingOne' data-parent='#accordion'>" +
+        "<div class='card-body'>" +
+        "<div class='list-group'>" +
+        "<a  class='list-group-item list-group-item-action flex-column align-items-start'>" +
+        "<div class='d-flex w-100 justify-content-between'>" +
+        "<h5 class='mb-1'>" + usuario + "</h5>" +
+        "<small>3 days ago</small>" +
+        "</div>" +
+        "<p class='mb-1'>" + comentario + "</p>" +
+        "<small><label class='estrellacoment' >" + estrellas + "</label></small>" +
+        "</a>" +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "</div>";
 
 }
