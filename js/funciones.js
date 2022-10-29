e1fc30d2c3909af18cc10536ac6aff0fe68762b8
Lizeth Zuñiga <111620498+lizethzu98@users.noreply.github.com>
2022-10-28 21:49:19 -0500

    local storage

--- js/funciones.js
+++ js/funciones.js
@@ -1,32 +1,41 @@
-document.body.onload = function () {
-    document.getElementById("cmbNomUsuario").value = "lizeth";
-    document.getElementById("selectPeliculas").value = "6";
-    document.getElementById("cmbComentario").value = "esta chida";
-    var arRadioBtn = document.getElementsByName("estrellas");
 
+var cmbNumUsuario = document.getElementById("cmbNomUsuario");
+var cmbComentario = document.getElementById("cmbComentario");
+var selectPeliculas = document.getElementById("selectPeliculas");
+var arRadioBtn = document.getElementsByName("estrellas");
+var idComentario = 0;
+localStorage.clear();
+let listaComentario = JSON.parse(localStorage.getItem('LC'));
+
+document.body.onload = function () {
+    cmbNumUsuario.value = "lizeth";
+    selectPeliculas.value = "6";
+    cmbComentario.value = "esta chida";
+    var arRadioBtn = document.getElementsByName("estrellas");
+    
     for (var ii = 0; ii < arRadioBtn.length; ii++) {
         var radButton = arRadioBtn[ii];
         radButton.checked = true;
     }
 }
 
-//id de comentario
-var idComentario = 0;
+
+
 
 function validaCampos() {
-    var usuario = document.getElementById("cmbNomUsuario").value;
-    var pelicula = document.getElementById("selectPeliculas").value;
-    var comentario = document.getElementById("cmbComentario").value;
+    let usuario = cmbNumUsuario.value;
+    let pelicula = selectPeliculas.value;
+    let comentario = cmbComentario.value;
     let calificacion = $('input[name="estrellas"]:checked').val();
-    //alerta
-    //Alertas
-    var alertas = 0;
+
+    //textosError
+    var textosError = 0;
     const divUsuario = document.getElementById("divUsuario");
-    alertas = divUsuario.getElementsByClassName('texto-error').length;
+    textosError = divUsuario.getElementsByClassName('texto-error').length;
 
     //Validar usuario
     if (usuario == '' || usuario == null || usuario.length <= 3) {
-        if (alertas <= 0) {
+        if (textosError <= 0) {
 
             let div = document.createElement("div");
             div.textContent = "Valor requerido*";
@@ -36,20 +45,20 @@ function validaCampos() {
         }
     }
     else {
-        if (alertas == 1) {
+        if (textosError == 1) {
             divUsuario.removeChild(divUsuario.lastChild);
 
         }
 
     }
 
-    //alertas
-    alertas = 0;
+    //textosError
+    textosError = 0;
     const divPeliculas = document.getElementById("divPeliculas");
-    alertas = divPeliculas.getElementsByClassName('texto-error').length;
+    textosError = divPeliculas.getElementsByClassName('texto-error').length;
     //validar peliculas
     if (pelicula == '' || pelicula == null || pelicula == 0) {
-        if (alertas == 0) {
+        if (textosError == 0) {
 
             let div = document.createElement("div");
             div.textContent = "Valor requerido*";
@@ -59,20 +68,20 @@ function validaCampos() {
         }
     }
     else {
-        if (alertas == 1) {
+        if (textosError == 1) {
             divPeliculas.removeChild(divPeliculas.lastChild);
         }
 
 
     }
-    alertas = 0;
+    textosError = 0;
     const divComentarios = document.getElementById("divComentarios");
-    alertas = divComentarios.getElementsByClassName('texto-error').length;
+    textosError = divComentarios.getElementsByClassName('texto-error').length;
 
     //validar comentarios
 
     if (comentario == '' || comentario == null) {
-        if (alertas == 0) {
+        if (textosError == 0) {
 
             let div = document.createElement("div");
             div.textContent = "Valor requerido*";
@@ -82,17 +91,17 @@ function validaCampos() {
         }
     }
     else {
-        if (alertas == 1) {
+        if (textosError == 1) {
             divComentarios.removeChild(divComentarios.lastChild);
         }
     }
 
-    alertas = 0;
+    textosError = 0;
     const divCalificacion = document.getElementById("divCalificacion");
-    alertas = divCalificacion.getElementsByClassName('texto-error').length;
+    textosError = divCalificacion.getElementsByClassName('texto-error').length;
     //validar calificacion
     if (calificacion == '' || calificacion == null || calificacion == 0) {
-        if (alertas == 0) {
+        if (textosError == 0) {
 
             let div = document.createElement("div");
             div.textContent = "Valor requerido*";
@@ -101,48 +110,73 @@ function validaCampos() {
         }
     }
     else {
-        if (alertas == 1) {
+        if (textosError == 1) {
             divCalificacion.removeChild(divCalificacion.lastChild);
         }
     }
 
     var formulario = document.getElementById("formulario");
-    alertas = formulario.getElementsByClassName("texto-error").length;
-    if (alertas > 0) {
-        alert("error");
-    }
-    else {
-
+    textosError = formulario.getElementsByClassName("texto-error").length;
+    if (textosError <= 0) {
         var divNoComentario = document.getElementById("noComentario");
         if (divNoComentario != null) {
             divNoComentario.remove();
         }
 
-        //comentarios
-        imprimirComentario();
-
-        document.getElementById("cmbNomUsuario").value = "";
-        document.getElementById("selectPeliculas").value = "0";
-        document.getElementById("cmbComentario").value = "";
-        var arRadioBtn = document.getElementsByName("estrellas");
-
-        for (var ii = 0; ii < arRadioBtn.length; ii++) {
-            var radButton = arRadioBtn[ii];
-            radButton.checked = false;
+        if (listaComentario != null) {
+            idComentario = listaComentario.length + 1;
         }
+        else {
+            idComentario = 1;
+        }
+        //comentarios
+        guardarLocalStorage(usuario, pelicula, comentario, calificacion);
+
+        limpiarcampos();
+
     }
 }
 
+function limpiarcampos() {
+    cmbNomUsuario.value = "";
+    selectPeliculas.value = "0";
+    cmbComentario.value = "";
 
+    for (var ii = 0; ii < arRadioBtn.length; ii++) {
+        var radButton = arRadioBtn[ii];
+        radButton.checked = false;
+    }
+}
+function guardarLocalStorage(usuario, pelicula, comentario, calificacion) {
+    
+    let objectComentario = {
+        idComentario: idComentario,
+        usuario: usuario,
+        pelicula: pelicula,
+        comentario: comentario,
+        calificacion: calificacion
+    }
+
+
+    if (listaComentario === null) {
+        listaComentario = [];
+        listaComentario.push(objectComentario);
+        localStorage.setItem('LC', JSON.stringify(listaComentario));
+        console.log(JSON.stringify(listaComentario));
+    }
+    else {
+        listaComentario.push(objectComentario);
+        localStorage.setItem('LC', JSON.stringify(listaComentario));
+        console.log(JSON.stringify(listaComentario));
+    }
+
+
+}
+
+
+//COMENTAR
 function imprimirComentario() {
-    var usuario = document.getElementById("cmbNomUsuario").value;
-    var pelicula = document.getElementById("selectPeliculas");
-    var peliculaTexto = pelicula.options[pelicula.selectedIndex].text;
-    var comentario = document.getElementById("cmbComentario").value;
-    let calificacion = $('input[name="estrellas"]:checked').val();
-
     var estrellas = "";
-
 
     switch (calificacion) {
         case '1':
@@ -170,7 +204,6 @@ function imprimirComentario() {
     }
 
 
-    idComentario = idComentario + 1;
 
     var coment = document.getElementById("accordion");
 
@@ -188,15 +221,15 @@ function imprimirComentario() {
         "<div class='d-flex w-100 justify-content-between'>" +
         "<h5 class='mb-1'>" + usuario + "</h5>" +
         "<div class= 'justify-content-end'>" +
-        "<button type='button' style='margin: 10px'onclick='editarComentario("+idComentario+")' class='btn btn-warning'>Editar</button>" + 
+        "<button type='button' style='margin: 10px'onclick='editarComentario(" + idComentario + ")' class='btn btn-warning'>Editar</button>" +
         "<button type='button' class='btn btn-danger'>Eliminar</button>" +
         "  </div> " +
         "</div>" +
-        "<div id='divTextoComentario"+idComentario+"'>"+
+        "<div  name='nameComentarios' id='divTextoComentario" + idComentario + "'>" +
         "<p class='mb-1'>" + comentario + "</p>" +
         "<small><label class='estrellacoment' >" + estrellas + "</label></small>" +
-        "</div>"+
-        "<div id='comentario"+idComentario+"'></div>" +
+        "</div>" +
+        "<div id='comentario" + idComentario + "'></div>" +
         "</a>" +
         "</div>" +
         "</div>" +
@@ -205,7 +238,7 @@ function imprimirComentario() {
 }
 
 
-
+//boton editar
 function editarComentario(nuComentario) {
     var usuario = document.getElementById("cmbNomUsuario").value;
     var pelicula = document.getElementById("selectPeliculas");
@@ -214,36 +247,33 @@ function editarComentario(nuComentario) {
     let calificacion = $('input[name="estrellas"]:checked').val();
 
 
-    var edit = document.getElementById("comentario"+nuComentario);
-    edit.innerHTML = 
-  "<form>"+
-    "<div class='form-group'>"+
-      "<label for='exampleInputEmail1'>Comentario</label>"+
-      "<input type='text' class='form-control' id='comentarioEdit'  placeholder='Escribe lo que piensas'>"+
-      "<label for='exampleInputEmail1'>Calificación<span style='color: red;'>*</span></label>"+
+    var edit = document.getElementById("comentario" + nuComentario);
+    edit.innerHTML =
+        "<form>" +
+        "<div class='form-group' id='comentariosEdit'>" +
+        "<label for='exampleInputEmail1'>Comentario</label>" +
+        "<input type='text' class='form-control' id='comentarioEdit'  placeholder='Escribe lo que piensas'></input>" +
+        "<label for='exampleInputEmail1'>Calificación<span style='color: red;'>*</span></label>" +
 
-      "<p class='clasificacion'>"+
-          "<input id='calValUnoEdit' type='radio' name='estrellasEdit' value='5'>"+
-          "<label class='estrella' for='calValUnoEdit'>★</label>"+
-          "<input id='calValDosEdit' type='radio' name='estrellasEdit' value='4'>"+
-          "<label class='estrella' for='calValDosEdit'>★</label>"+
-          "<input id='calValTresEdit' type='radio' name='estrellasEdit' value='3'>"+
-          "<label class='estrella' for='calValTresEdit'>★</label>"+
-          "<input id='calValCuatroEdit' type='radio' name='estrellasEdit' value='2'>"+
-          "<label class='estrella' for='alValCuatroEdit'>★</label>"+
-          "<input id='calValCincoEdit' type='radio' name='estrellasEdit' value='1'>"+
-          "<label class='estrella' for='calValCincoEdit'>★</label>"+
-      "</p>"+
-    "</div>"+
-    "<button type='submit' class='btn btn-primary'>Actualizar</button>"+
-  "</form>";
+        "<p class='clasificacion'>" +
+        "<input id='calValUnoEdit' type='radio' name='estrellasEdit' value='5'>" +
+        "<label class='estrella' for='calValUnoEdit'>★</label>" +
+        "<input id='calValDosEdit' type='radio' name='estrellasEdit' value='4'>" +
+        "<label class='estrella' for='calValDosEdit'>★</label>" +
+        "<input id='calValTresEdit' type='radio' name='estrellasEdit' value='3'>" +
+        "<label class='estrella' for='calValTresEdit'>★</label>" +
+        "<input id='calValCuatroEdit' type='radio' name='estrellasEdit' value='2'>" +
+        "<label class='estrella' for='alValCuatroEdit'>★</label>" +
+        "<input id='calValCincoEdit' type='radio' name='estrellasEdit' value='1'>" +
+        "<label class='estrella' for='calValCincoEdit'>★</label>" +
+        "</p>" +
+        "</div>" +
+        "<button type='submit' class='btn btn-primary'>Actualizar</button>" +
+        "</form>";
 
-    var  divTextoComentario = document.getElementById("divTextoComentario"+nuComentario);
+    var divTextoComentario = document.getElementById("divTextoComentario" + nuComentario);
     var textoComentarioEdit = divTextoComentario.getElementsByTagName("p")[0].innerHTML;
     document.getElementById("comentarioEdit").value = textoComentarioEdit;
-
-    divTextoComentario.innerHTML="";
-  
-
+    divTextoComentario.innerHTML = "";
 
 }
